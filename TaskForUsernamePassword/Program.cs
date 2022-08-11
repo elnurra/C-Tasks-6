23edbc7d49445ef4c591db14d3ea25ab24bfa360
elnurra <elnurra@code.edu.az>
2022-08-11 15:09:42 +0400

    Task successfully completed

---
+++ TaskForUsernamePassword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskForUsernamePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("Input your username: ");
+            string username = Console.ReadLine();
+            user._userName = username;
+            
+
+
+            Console.WriteLine("Input you password: ");
+            string password = Console.ReadLine();
+            user._password = password;
+            
+            user.showInfo();
+            
+        }
+    }
+}
---
+++ TaskForUsernamePassword/User.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskForUsernamePassword
+{
+    class User
+    {
+        private string UserName ;
+        private string Password ;
+        public string _userName
+        {
+            get
+            {
+                return UserName;
+            }
+            set
+            {
+                if (value.Length>5 && value.Length<26)
+                {
+                    UserName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input (lenthg) UserName: ");
+                }
+                           
+            } 
+            
+        }
+        public string _password 
+        { 
+            get 
+            {
+                return Password;
+            }
+            set
+            {
+                if (value.Length>7 && value.Length<26 && HasDigits(value) && HasLower(value) && HasUpper(value))
+                {
+                    
+                    
+                        Password=value;
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input password: ");
+                }
+               
+
+            }
+         }
+         
+            private bool HasDigits(string hasdigits) 
+            {
+
+            foreach (char item in hasdigits)
+            {
+                if (char.IsDigit(item))
+                {
+                    return true;
+                }
+
+            }
+            
+            return false;
+        
+             }
+            private bool HasUpper(string hasupper) 
+             {
+            
+            foreach (char item in hasupper)
+            {
+                if (char.IsUpper(item))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+             }
+            private bool HasLower(string haslower)
+            {
+
+            foreach (char item in haslower)
+            {
+                if (char.IsLower(item))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public void showInfo() 
+        {
+            Console.WriteLine($"Username: {UserName} \n Password: {Password} ");
+        
+        }
+        
+
+
+    }
+    }
+
+
+        //public int ChangeNumber { 
+        //    set 
+        //    {
+        //        if (value > 0) 
+        //        {
+        //            Password = value;
+        //        }
+                
+        //    } 
+        //    get 
+        //    {
+                
+        //        return Password;
+        //    }
+        //private int Password=48;
+
+        //public void SetChangePassword(int number) 
+        //{
+        //    if (number>0)
+        //    {
+        //        Password = number;
+        //    }
+           
+        
+        //}
+        //public int GetChangePassword()
+        //{
+        //    return Password;
+
+        //}
